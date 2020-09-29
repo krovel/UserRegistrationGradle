7ded51900600f77d14ad85af911b5b540666fbb5
krove <krove@DESKTOP-Q5SED1R>
2020-09-30 01:50:17 +0530

    [Kashif]UC2 added. Last name validity

--- src/main/java/com/capgemini/UserRegistration.java
+++ src/main/java/com/capgemini/UserRegistration.java
@@ -20,9 +20,22 @@ public class UserRegistration {
         else
         	System.out.println("ERROR ! Requirement not satisfied.");
 	}
+	public void lname() {
+		System.out.println("Enter your last name (NOTE: First letter should be capital and shuold have minimum 3 characters) :");
+        String first = sc.next();
+		Pattern p = Pattern.compile("^[A-Z][a-zA-Z]{2,}$");
+		Matcher m = p.matcher(first);
+		Boolean check = m.find();
+        if(check) {
+        	System.out.println("Valid last name.");
+        }
+        else
+        	System.out.println("ERROR ! Requirement not satisfied.");
+	}
 	public static void main(String[] args) {
 		System.out.println("Welcome to User Registration.");
 		UserRegistration u = new UserRegistration();
 		u.fname();
+		u.lname();
 	}
 }
