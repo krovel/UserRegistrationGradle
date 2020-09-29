8709799cfe82a85e552d94b7d1bff9f820125d4e
krove <krove@DESKTOP-Q5SED1R>
2020-09-30 03:05:43 +0530

    [Kashif]UC10 added. JUnit testing

--- src/main/java/com/capgemini/UserRegistration.java
+++ src/main/java/com/capgemini/UserRegistration.java
@@ -8,9 +8,9 @@ import java.util.regex.*;
 
 public class UserRegistration {
 	Scanner sc=new Scanner(System.in);
-	public void fname() {
-		System.out.println("Enter your first name (NOTE: First letter should be capital and shuold have minimum 3 characters) :");
-        String first = sc.next();
+	public boolean fname(String first) {
+		//System.out.println("Enter your first name (NOTE: First letter should be capital and shuold have minimum 3 characters) :");
+        //String first = sc.next();
 		Pattern p = Pattern.compile("^[A-Z][a-zA-Z]{2,}$");
 		Matcher m = p.matcher(first);
 		Boolean check = m.find();
@@ -19,23 +19,24 @@ public class UserRegistration {
         }
         else
         	System.out.println("ERROR ! Requirement not satisfied.");
-        	first=sc.next();
+        return check;
 	}
-	public void lname() {
-		System.out.println("Enter your last name (NOTE: First letter should be capital and shuold have minimum 3 characters) :");
-        String first = sc.next();
+	public boolean lname(String last) {
+		//System.out.println("Enter your last name (NOTE: First letter should be capital and shuold have minimum 3 characters) :");
+        //String first = sc.next();
 		Pattern p = Pattern.compile("^[A-Z][a-zA-Z]{2,}$");
-		Matcher m = p.matcher(first);
+		Matcher m = p.matcher(last);
 		Boolean check = m.find();
         if(check) {
         	System.out.println("Valid last name.");
         }
         else
         	System.out.println("ERROR ! Requirement not satisfied.");
+        return check;
 	}
-	public void email() {
-		System.out.println("Enter your Email:");
-        String email = sc.next();
+	public boolean email(String email) {
+		//System.out.println("Enter your Email:");
+        //String email = sc.next();
 		Pattern p = Pattern.compile("^abc+([_+-.]{0,1}([a-zA-Z0-9]+))*[@][a-z0-9]{1,}[.]([c][o][m]|[n][e][t])([.][a-zA-Z]{2,}){0,1}$");
 		Matcher m = p.matcher(email);
 		Boolean check = m.find();
@@ -43,12 +44,13 @@ public class UserRegistration {
         	System.out.println("Valid Email.");
         }
         else
-        	System.out.println("ERROR ! Requirement not satisfied.");  
+        	System.out.println("ERROR ! Requirement not satisfied.");
+		return check;
 	}
-	public void mobile() {
-		Scanner in=new Scanner(System.in);
-		System.out.println("Enter your Mobile No:");
-		String mobile = in.nextLine();
+	public boolean mobile(String mobile) {
+		//Scanner in=new Scanner(System.in);
+		//System.out.println("Enter your Mobile No:");
+		//String mobile = in.nextLine();
 		Pattern p = Pattern.compile("^[0-9]{2}[ ][1-9][0-9]{9}$");
 		Matcher m = p.matcher(mobile);
 		Boolean check = m.find();
@@ -56,11 +58,12 @@ public class UserRegistration {
         	System.out.println("Valid Mobile Number.");
         }
         else
-        	System.out.println("ERROR ! Requirement not satisfied.");  
+        	System.out.println("ERROR ! Requirement not satisfied.");
+		return check;
 	}
-	public void pwd() {
-		System.out.println("Enter your Password:");
-		String pwd = sc.next();
+	public boolean pwd(String pwd) {
+		//System.out.println("Enter your Password:");
+		//String pwd = sc.next();
 		Pattern p = Pattern.compile("(?=.*[A-Z])(?=.*\\d)(?=.*\\W)(?!.*\\W\\w*\\W)(?!.*\\s).{5,}$");
 		Matcher m = p.matcher(pwd);
 		Boolean check = m.find();
@@ -68,15 +71,22 @@ public class UserRegistration {
         	System.out.println("Valid Password.");
         }
         else
-        	System.out.println("ERROR ! Requirement not satisfied.");  
+        	System.out.println("ERROR ! Requirement not satisfied.");
+		return check;
+	}
+	public String analyseMood(String message) {
+		if (message.contains("Happy"))
+			return "Happy";
+		else
+			return "SAD";
 	}
 	public static void main(String[] args) {
 		System.out.println("Welcome to User Registration.");
-		UserRegistration u = new UserRegistration();
+	/*	UserRegistration u = new UserRegistration();
 		u.fname();
 		u.lname();
 		u.email();
 		u.mobile();
-		u.pwd();
+		u.pwd(); */
 	}
 }
--- src/test/java/com/capgemini/UserRegistrationTest.java
+++ src/test/java/com/capgemini/UserRegistrationTest.java
@@ -3,12 +3,73 @@
  */
 package com.capgemini;
 
-import org.junit.Test;
-
 import com.capgemini.UserRegistration;
 
-import static org.junit.Assert.*;
+import org.junit.*;
 
 public class UserRegistrationTest {
 	
+	@Test
+    public void givenFirstName_WhenValid_ReturnTrue() {
+        UserRegistration user = new UserRegistration();
+        Assert.assertTrue(user.fname("Kashif"));
+    }
+ @Test
+    public void givenFirstName_WhenInvalid_ReturnFalse() {
+        UserRegistration user = new UserRegistration();
+        Assert.assertFalse(user.fname("kashif"));
+    }
+ @Test
+    public void givenLastName_WhenValid_ReturnTrue() {
+        UserRegistration user = new UserRegistration();
+        Assert.assertTrue(user.lname("Ansari"));
+    }
+ @Test
+    public void givenLastName_WhenInvalid_ReturnFalse() {
+        UserRegistration user = new UserRegistration();
+        Assert.assertFalse(user.lname("ansari"));
+    }
+ @Test
+    public void givenEmail_WhenValid_ReturnTruee() {
+        UserRegistration user = new UserRegistration();
+        Assert.assertTrue(user.email("abc@gmail.com"));
+    }
+ @Test
+    public void givenEmail_WhenInvalid_ReturnFalse() {
+        UserRegistration user = new UserRegistration();
+        Assert.assertFalse(user.email("kashif"));
+    } 
+ @Test
+    public void givenMobileNo_WhenValid_ReturnTrue() {
+        UserRegistration user = new UserRegistration();
+        Assert.assertTrue(user.mobile("91 8737933455"));
+    }
+ @Test
+    public void givenMobileNo_WhenInvalid_ReturnFalse() {
+        UserRegistration user = new UserRegistration();
+        Assert.assertFalse(user.mobile("12345"));
+    }
+ @Test
+    public void givenPassword_WhenValid_ReturnTrue() {
+        UserRegistration user = new UserRegistration();
+        Assert.assertTrue(user.pwd("Kashif1@"));
+    }
+ @Test
+    public void givenPassword_WhenInvalid_ReturnFalse() {
+        UserRegistration user = new UserRegistration();
+        Assert.assertFalse(user.pwd("kas"));
+    }
+ @Test
+	public void givenMessage_whenSad_ShouldReturnSad() {
+	 	UserRegistration user = new UserRegistration();
+		String mood = user.analyseMood("This is a Sad Message");
+		Assert.assertEquals("SAD", mood);
+	}
+
+	@Test
+	public void givenMessage_whenNotSad_ShouldReturnHappy() {
+		UserRegistration user = new UserRegistration();
+		String mood = user.analyseMood("This is a Happy Message");
+		Assert.assertEquals("Happy", mood);
+	}
 }
